92f28c4ce4f21072decd05c745083915a95bd84b
fsy-juruo <42823084+fsy-juruo@users.noreply.github.com>
2019-11-14 22:10:19 +0800

    add starter.cpp
    最基本的代码起手式

---
+++ starter.cpp
@@ -0,0 +1,28 @@
+#include <bits/stdc++.h>
+#define _rep(i, x, y) for(int i = x; i <= y; i++)
+#define _per(i, x, y) for(int i = x; i >= y; i--)
+#define LL long long
+using namespace std;
+template <typename T>
+inline void read(T &x) {
+    x = 0; T f = (T) 1;
+    char c = getchar();
+    for (; !isdigit(c); c = getchar())
+        if (c == '-') f = -f;
+    for (; isdigit(c); c = getchar()) x = x * 10 + c - 48;
+    x *= f;
+}
+template <typename T>
+inline void write(T x) {
+	if(x < 0) {putchar('-'); x = -x;}
+	if(x > 9) write(x / 10);
+	putchar(x % 10 + '0');
+}
+template <typename T>
+inline void writesp(T x, char sp = '\n') {
+	write(x); putchar(sp);
+}
+int main() {
+
+	return 0;
+}
